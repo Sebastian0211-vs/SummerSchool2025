1d4d9e69af4f7bb7a64b4d3b94bd974b3cdc5de6
zcAntZuf <anthony.zufferey@zeacold.com>
2025-08-19 19:32:41 +0200

    feat: Display a simple triangle at the middle of the screen

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from src.midi.models import Note
 from src.midi.parser import parse_midi
 import os
-
+from src.ui.audio_visualizer import AudioVisualizer
 from src.audio.AudioSplitter_v1 import AudioSplit
+
 
 def main():
 
@@ -12,17 +13,17 @@ def main():
     piano_wav_out = os.path.join(res_dir, "temp", "piano.wav")
     trumpet_wav_out = os.path.join(res_dir, "temp", "trumpet.wav")
 
-
     print("Input file:", input_file_mp3)
     print("Piano output file:", piano_wav_out)
     print("Trumpet output file:", trumpet_wav_out)
 
-
     AudioSplit(input_file_mp3, piano_wav_out, trumpet_wav_out)
+
+    visualizer = AudioVisualizer()
+    visualizer.run()
+
     pass
 
 
 if __name__ == "__main__":
-
-
     main()
