8a04c4479d2fc06ee37d9e60cd8fd49ea19a6a04
zedbros <jameszed@icloud.com>
2025-08-27 12:56:59 +0200

    Slight refactoring

---
+++ src/audio/main.py
@@ -0,0 +1,20 @@
+import librosa
+import time
+from sound_to_midi.monophonic import wave_to_midi_poly
+
+filename =  'src/audio/res/samples/SebPPp2.wav'
+fileout =   'src/audio/res/midiOutputs/testPolyphony.mid'
+
+y, sr = librosa.load(filename, sr=None)
+'''
+Using git's sound_to_midi that utilizes librosa
+So attempting to improve it in order to have a
+more accurate transposer.
+'''
+timeStart = time.time()
+midi = wave_to_midi_poly(y, sr)
+with open(fileout, 'wb') as f:
+    midi.writeFile(f)
+timeEnd = time.time() - timeStart
+
+print("/////////////FINISHED IN ", timeEnd, "SECONDS///////////////////")
--- src/audio/tester.py
+++ src/audio/tester.py
@@ -1,16 +1,7 @@
 import librosa
-import math
+import librosa.feature.rhythm
 
-filename = 'src/audio/res/samples/PinkPanther_Both.mp3'
-#filename = "src/audio/res/samples/wavSample.wav"
+
+filename =  'src/audio/res/samples/SebPPp2.wav'
 y, sr = librosa.load(filename, sr=None)
-#print(y)
-#print(sr)
-
-tempo, beatFrames = librosa.beat.beat_track(y=y, sr=sr)
-#beatTimes = librosa.frames_to_time(beatFrames, sr=sr)
-#print(beatFrames)
-#print(format(tempo))
-#print(beatTimes)
-audioBPM = (tempo)
-print("This be my bpm estimate: ", audioBPM)
+bpm = librosa.feature.rhythm.tempo(y=y, sr=sr)
