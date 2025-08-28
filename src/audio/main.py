34b2df5af693472cfaaf3782dbbf29a6085589c3
zedbros <jameszed@icloud.com>
2025-08-28 23:50:16 +0200

    REFACTOR

--- src/audio/main.py
+++ src/audio/main.py
@@ -1,13 +1,13 @@
 import time
-from soundToMidiLocalLibrary.sound_to_midi.mainOptimised import proceed
+from mp3_to_midi_converter import proceed
 
 filename =  'src/audio/res/samples/premade/PinkPanther_Piano_Only.mp3'
-fileout  =  'src/audio/res/midiOutputs/testClaudePolyphony.mid'
+fileout  =  'src/audio/res/midiOutputs/RV0.mid'
 
 PPp      =  'src/audio/res/samples/premade/PinkPanther_Piano_Only.mp3'
 PPt      =  'src/audio/res/samples/premade/PinkPanther_Trumpet_Only.mp3'
 
-midi = proceed(PPt, False)
+midi = proceed(filename, False)
 with open(fileout, 'wb') as f:
     midi.writeFile(f)
 
--- src/audio/tester.py
+++
@@ -1,7 +0,0 @@
-import librosa
-import librosa.feature.rhythm
-
-
-filename =  'src/audio/res/samples/SebPPp2.wav'
-y, sr = librosa.load(filename, sr=None)
-bpm = librosa.feature.rhythm.tempo(y=y, sr=sr)
